f940c2e46359c387f40a4c5c866f55bfddea22ff
David Kanenwisher <d.kanen@gmail.com>
2020-06-15 19:57:47 -0500

    Initial Commit

---
+++ MetalTriangles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MetalTriangles
+//
+//  Created by David Kanenwisher on 6/15/20.
+//  Copyright © 2020 dkanen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
